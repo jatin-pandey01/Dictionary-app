fcef61cc8307a546a46e8d5464de6454e1ff7f98
jatin-pandey01 <=>
2024-05-24 16:06:12 +0530

    Dictionary model added

---
+++ lib/dictionary_model.dart
@@ -0,0 +1,72 @@
+class DictionaryModel {
+    String word;
+    List<Phonetic> phonetics;
+    List<Meaning> meanings;
+    License license;
+    List<String> sourceUrls;
+
+    DictionaryModel({
+        required this.word,
+        required this.phonetics,
+        required this.meanings,
+        required this.license,
+        required this.sourceUrls,
+    });
+
+}
+
+class License {
+    String name;
+    String url;
+
+    License({
+        required this.name,
+        required this.url,
+    });
+
+}
+
+class Meaning {
+    String partOfSpeech;
+    List<Definition> definitions;
+    List<String> synonyms;
+    List<String> antonyms;
+
+    Meaning({
+        required this.partOfSpeech,
+        required this.definitions,
+        required this.synonyms,
+        required this.antonyms,
+    });
+
+}
+
+class Definition {
+    String definition;
+    List<dynamic> synonyms;
+    List<dynamic> antonyms;
+    String example;
+
+    Definition({
+        required this.definition,
+        required this.synonyms,
+        required this.antonyms,
+        required this.example,
+    });
+
+}
+
+class Phonetic {
+    String audio;
+    String sourceUrl;
+    License license;
+    String text;
+
+    Phonetic({
+        required this.audio,
+        required this.sourceUrl,
+        required this.license,
+        required this.text,
+    });
+
+}
--- lib/services.dart
+++ lib/services.dart
@@ -1,5 +1,15 @@
+import 'package:dictionary_app/dictionary_model.dart';
 import 'package:http/http.dart' as http;
 
 class ApiServices{
   String baseUrl = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+  Future<DictionaryModel> fetchData(String word) async{
+    Uri url = Uri.parse('${baseUrl}${word}');
+    final res = await http.get(url);
+    try {
+      
+    } catch (e) {
+      
+    }
+  }
 }
